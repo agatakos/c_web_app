b3575530a80da3d5939e1588d67561c9db686901
Agata Kostrzewa <agata.kostrzewa@wework.com@Agatas-MacBook-Pro.local>
2021-05-09 22:01:15 +0100

    config change

--- instance/config.py
+++ instance/config.py
@@ -1,19 +1,34 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     """Parent configuration class."""
 
     DEBUG = False
+    TESTING = False
     CSRF_ENABLED = True
     SECRET = os.getenv("SECRET")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 
-class DevelopmentConfig(Config):
-    """Configurations for Development."""
+class ProductionConfig(Config):
+    DEBUG = False
 
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
     DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
 
 
 app_config = {
